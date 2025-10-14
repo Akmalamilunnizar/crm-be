e18659ef52bd9321483e2e1cb0f9facba9dd27c8
Nomikuza <uzumakisujai@gmail.com>
2025-10-14 11:24:59 +0700

    Fix database configuration to use individual environment variables

--- internal/config/database/mysql.go
+++ internal/config/database/mysql.go
@@ -17,7 +17,23 @@ func DatabaseMysql() *gorm.DB {
 		log.Fatal("Error loading .env file")
 	}
 
-	dsn := os.Getenv("MYSQL")
+	// Get database configuration from environment variables
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "3306"
+	}
+
+	// Create DSN from individual variables
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	
+	log.Println("Connecting to database:", dbHost+":"+dbPort)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {

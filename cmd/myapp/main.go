7b286c7444e8de2b0f8ac6b016a304e9b79460b5
Nomikuza <uzumakisujai@gmail.com>
2025-10-24 12:28:32 +0700

    fix (CORS): add rndpolije.lilly.net.id to allowed origins for enhanced access control

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -29,7 +29,7 @@ func main() {
 	})
 	// CORS configuration
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, http://127.0.0.1:3000, http://192.168.1.7:3000, http://192.168.1.11:3000, http://localhost:3002, http://127.0.0.1:3002, http://192.168.1.11:3002, https://menara.net.id",
+		AllowOrigins:     "http://localhost:3000, http://127.0.0.1:3000, http://192.168.1.7:3000, http://192.168.1.11:3000, http://localhost:3002, http://127.0.0.1:3002, http://192.168.1.11:3002, https://menara.net.id, http://rndpolije.lilly.net.id, https://rndpolije.lilly.net.id, http://rndpolije.lilly.net.id:3000, https://rndpolije.lilly.net.id:3000",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true,
